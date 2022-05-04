238e84e9c276d030b90f8bf97893a87d1eb36ef1
RichardKT88 <kendy_t88@hotmail.com>
2022-05-04 16:01:19 -0300

    [Done] Search a Product on the Server

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -3,26 +3,26 @@
     public class ProductService : IProductService
     {
         private readonly DataContext _context;
-            
+
         public ProductService(DataContext context)
-        {   
+        {
             _context = context;
         }
 
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
-           var response = new ServiceResponse<Product>();
-           var product = await _context.Products
-                .Include(p => p.Variants)
-                .ThenInclude(v => v.ProductType)
-                .FirstOrDefaultAsync(p => p.Id == productId);
+            var response = new ServiceResponse<Product>();
+            var product = await _context.Products
+                 .Include(p => p.Variants)
+                 .ThenInclude(v => v.ProductType)
+                 .FirstOrDefaultAsync(p => p.Id == productId);
 
-            if (product == null) 
+            if (product == null)
             {
                 response.Success = false;
                 response.Message = "Sorry, but this product does not exist.";
             }
-            else 
+            else
             {
                 response.Data = product;
             }
@@ -38,7 +38,7 @@
                 .Include(p => p.Variants)
                 .ToListAsync()
             };
-            
+
             return response;
         }
 
@@ -53,6 +53,21 @@
             };
             return response;
 
+        }
+
+        public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await _context.Products
+                    .Where(p => p.Title.ToLower().Contains(searchText.ToLower())
+                    ||
+                    p.Description.ToLower().Contains(searchText.ToLower()))
+                    .Include(p => p.Variants)
+                    .ToListAsync()
+            };
+
+            return response;
         }
     }
 }
