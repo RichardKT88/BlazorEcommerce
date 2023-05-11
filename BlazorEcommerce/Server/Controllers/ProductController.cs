67d0eae6927161f980b9076a080231d4f1e994e7
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2023-05-11 16:35:11 -0300

    Section 4: 60. Implement the Search Component

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -1,6 +1,4 @@
-﻿using BlazorEcommerce.Server.Services.ProductService;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers
 {
@@ -15,7 +13,7 @@ namespace BlazorEcommerce.Server.Controllers
             _productService = productService;
         }
 
-        [HttpGet] 
+        [HttpGet]
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct()
         {
             var result = await _productService.GetProductsAsync();
@@ -34,6 +32,20 @@ namespace BlazorEcommerce.Server.Controllers
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductsByCategory(string categoryUrl)
         {
             var result = await _productService.GetProductsByCategory(categoryUrl);
+            return Ok(result);
+        }
+
+        [HttpGet("search/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> SearchProducts(string searchText)
+        {
+            var result = await _productService.SearchProducts(searchText);
+            return Ok(result);
+        }
+
+        [HttpGet("searchsuggestions/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _productService.GetProductSearchSuggestions(searchText);
             return Ok(result);
         }
     }
---
+++ BlazorEcommerce/Shared/ProductType.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Shared
+{
+    public class ProductType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        /* [NotMapped]
+         public bool Editing { get; set; } = false;
+         [NotMapped]
+         public bool IsNew { get; set; } = false;*/
+    }
+}
